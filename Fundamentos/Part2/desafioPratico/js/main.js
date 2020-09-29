cda6db62de623c62636cee1d3ebcbad4840efceb
Kamila-Vieira <vkamila54@outlook.com>
2020-09-29 12:08:43 -0300

    Finished Part2

--- Fundamentos/Part2/desafioPratico/js/main.js
+++ Fundamentos/Part2/desafioPratico/js/main.js
@@ -1,111 +1,176 @@
 "use strict";
 // Variaveis de estado
 let cardListContainer = null;
-let onSelectRadioAnd = null;
-let onSelectRadioOr = null;
-let onSelectCheckJava = null;
-let onSelectCheckJavascript = null;
-let onSelectCheckPython = null;
-let inputSearch = null;
-let buttonSearch = null;
+let radioElements = null;
+let radioElementsChecked = null;
+let checkboxesElements = null;
+let inputElement = null;
 let allDevs = [];
 let languages = [];
 let countDevs = 0;
+let filteredLanguages = [];
+let valueToSearch = "";
 
 window.addEventListener("load", async () => {
-  await getDevsList();
-
-  filterDevs();
-  searchDevs();
+  getFilterElements();
+  await fetchDevs();
+  addEvent();
 });
 
-const getDevsList = async () => {
+function getFilterElements() {
+  inputElement = document.querySelector(".input-search");
+  radioElements = document.querySelectorAll("[name=radio-option]");
+  checkboxesElements = document.querySelectorAll("[name=language-types]");
+  radioElementsChecked = document.querySelectorAll(
+    "[name=language-types]:checked"
+  ).id;
+  checkboxesElements.forEach((language) =>
+    language.checked ? filteredLanguages.push(language.id) : null
+  );
+}
+
+function removeAccentsAndSpaces(name) {
+  return name.normalize("NFD").replace(/[^a-zA-Zs]/g, "");
+}
+
+const fetchDevs = async () => {
   const res = await fetch("http://localhost:3001/devs");
   const json = await res.json();
-  //console.log(json);
   allDevs = json.map((dev) => {
     const { id, name, picture, programmingLanguages } = dev;
+    const nameLowerCase = name.toLowerCase();
+    const nameToSearch = removeAccentsAndSpaces(nameLowerCase);
     return {
       id,
       name,
+      nameToSearch,
       picture,
-      languages: programmingLanguages,
+      programmingLanguages,
     };
   });
-  createDevCard();
+
+  allDevs = allDevs.map((dev) => {
+    const { id, name, nameToSearch, picture, programmingLanguages } = dev;
+    const languagesTypes = [];
+    const programmingLanguageIcons = [];
+    programmingLanguages.forEach((languages) => {
+      const { language, experience, id } = languages;
+      const idLowerCase = id.toLowerCase();
+      languagesTypes.push(language.toLowerCase());
+      switch (language) {
+        case "Java":
+          programmingLanguageIcons.push({
+            idLowerCase,
+            language,
+            experience,
+            icon: "./assets/img/language/java.png",
+          });
+          break;
+        case "JavaScript":
+          programmingLanguageIcons.push({
+            idLowerCase,
+            language,
+            experience,
+            icon: "./assets/img/language/javascript.png",
+          });
+          break;
+        case "Python":
+          programmingLanguageIcons.push({
+            idLowerCase,
+            language,
+            experience,
+            icon: "./assets/img/language/python.png",
+          });
+          break;
+        default:
+          break;
+      }
+    });
+    return {
+      id,
+      name,
+      nameToSearch,
+      picture,
+      languagesTypes,
+      programmingLanguageIcons,
+    };
+  });
+  createDevCard(allDevs);
 };
 
-const createDevCard = () => {
+const createDevCard = (devCard) => {
   cardListContainer = document.querySelector("#cards-container");
   let devsHTML = "<div class='devs'>";
+  countDevs = document.querySelector("#count-devs");
 
-  allDevs.forEach((dev) => {
-    const { id, name, picture, languages } = dev;
-    const { language } = languages;
-    const selectLanguage = () => {
-      for (let i = 0; i < languages.length; i++) {
-        return (languages[
-          i
-        ] = `<img src="./assets/img/language/${language}.png" alt="${language}">`);
-      }
-    };
-
-    const selectedLanguage = selectLanguage(language);
-    console.log(selectedLanguage);
-    const cardHTML = `
-      <div id="${id}" class="dev">
-        <img src='${picture}' alt="${name}" class="avatar">  
-        <div class="language-container">
-          <h6>${name}</h6>
-          <div class="language">
-            ${selectedLanguage}
+  if (devCard === null) {
+    countDevs.textContent = "0";
+  } else {
+    devCard.forEach((dev) => {
+      const { id, name, picture, programmingLanguageIcons } = dev;
+      let languages = "";
+      programmingLanguageIcons.forEach(({ icon, language }) => {
+        languages = `${languages} <img class='language-icon' src="${icon}" alt="${language}"></img>`;
+      });
+      const cardHTML = `
+        <div id="${id}" class="dev">
+          <img src='${picture}' alt="${name}" class="avatar">  
+          <div class="language-container">
+            <h6>${name}</h6>
+            <div class="language">
+              ${languages}
+            </div>
           </div>
         </div>
-      </div>
-    `;
-    devsHTML += cardHTML;
-  });
-  cardListContainer.innerHTML = devsHTML;
+      `;
+      devsHTML += cardHTML;
+    });
+    cardListContainer.innerHTML = devsHTML;
+    countDevs.textContent = devCard.length;
+  }
 };
 
-function checkedLanguages() {
-  selectLanguage(language);
+function addEvent() {
+  inputElement.addEventListener("input", handleKeyUp);
+  checkboxesElements.forEach((checkBoxElement) => {
+    checkBoxElement.addEventListener("input", checkedLanguages);
+  });
+  radioElements.forEach((radioElement) => {
+    radioElement.addEventListener("input", checkedRadios);
+  });
 }
 
 const filterDevs = () => {
-  const filterDevsByLanguage = allDevs.filter((dev) => {
-    const { id, name, picture, languages } = dev;
-    onSelectCheckJava = document.querySelector("#java");
-    onSelectCheckJavascript = document.querySelector("#javascript");
-    onSelectCheckPython = document.querySelector("#python");
-    onSelectRadioAnd = document.querySelector("#and");
-    onSelectRadioOr = document.querySelector("#or");
-
-    //onSelectCheckJava.addEventListener("checked", listener);
+  let filteredDevs = [];
+  const filterTextLowerCase = valueToSearch.toLocaleLowerCase();
+  filteredDevs = allDevs.filter((dev) => {
+    return dev.nameToSearch.includes(filterTextLowerCase);
   });
-
-  const totalDevs = () => {
-    countDevs = document.querySelector("#count-devs");
-    countDevs.textContent = filterDevsByLanguage.length || "0";
-  };
-  totalDevs();
+  filteredDevs = filteredDevs.filter((dev) => {
+    return radioElementsChecked === "or"
+      ? filteredLanguages.some((item) => dev.languagesTypes.includes(item))
+      : dev.languagesTypes.join("") === filteredLanguages.join("");
+  });
+  createDevCard(filteredDevs);
 };
 
-const searchDevs = (name) => {
-  inputSearch = document.querySelector(".input-search");
-  buttonSearch = document.querySelector(".button-search");
-  let nameLowerCase = [];
-  allDevs = allDevs.find((dev) => {
-    const { name } = dev;
-    nameLowerCase = name.toLowerCase().indexOf(name.toLowerCase()) > -1;
-    function onButtonPress() {
-      inputSearch.addEventListener("click");
-      if (inputSearch.value === nameLowerCase) {
-        return devsHTML;
-      }
-      return (countDevs.textContent = "0");
-    }
-    buttonSearch.addEventListener("press", onButtonPress);
+function handleKeyUp(event) {
+  valueToSearch = event.target.value;
+  filterDevs();
+}
+
+function checkedLanguages() {
+  filteredLanguages = [];
+  const checkboxesElementsCheck = document.querySelectorAll(
+    "[name=language-types]:checked"
+  );
+  checkboxesElementsCheck.forEach(({ id }) => {
+    filteredLanguages.push(id);
   });
-  console.log(nameLowerCase);
-};
+  filterDevs();
+}
+
+function checkedRadios(event) {
+  radioElementsChecked = event.target.id;
+  filterDevs();
+}
