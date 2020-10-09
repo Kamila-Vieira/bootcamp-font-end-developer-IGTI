94c4758a9430c72d70557843de4e64297a5fd1d0
Kamila-Vieira <vkamila54@outlook.com>
2020-10-09 13:06:53 -0300

    Update first changes

--- Angular/Part2/desafiopratico/src/app/app.module.ts
+++ Angular/Part2/desafiopratico/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { SenatorListComponent } from './senator-list/senator-list.component';
+import { SenatorItemComponent } from './senator-item/senator-item.component';
+registerLocaleData(localePt);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, SenatorListComponent, SenatorItemComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Angular/Part2/desafiopratico/src/app/senator-list/senator-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-senator-list',
+  templateUrl: './senator-list.component.html',
+  styleUrls: ['./senator-list.component.css']
+})
+export class SenatorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
