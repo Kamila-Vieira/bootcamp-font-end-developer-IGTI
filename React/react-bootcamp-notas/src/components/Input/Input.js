5167118847f4a193dcbb3e0a57658b5581520fa7
Kamila-Vieira <vkamila54@outlook.com>
2020-10-22 13:12:15 -0300

    update atividade pratica

--- React/react-bootcamp-notas/src/components/Input/Input.js
+++ React/react-bootcamp-notas/src/components/Input/Input.js
@@ -1,88 +1,27 @@
 import React, { Component } from "react";
 import style from "./style/style.module.css";
+//import Notas from "../Notas/Notas";
 
 export default class Input extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      firstInput: null,
-      secondInput: 0,
-      thirthInput: 0,
-      fourthInput: 0,
-      fivethInput: 0,
-    };
-  }
-
   handleInputsChange = (event) => {
-    this.setState({
-      firstInput: event.target.value,
-    });
-    this.props.onInputChange();
+    const { id } = this.props.currentGrades;
+    const grade = Number.parseInt(event.target.value, 10) || 0;
+    this.props.onGradeChange(grade, id);
   };
 
-  /* handleInputsValue = () => {
-    this.props.value();
-  }; */
-
   render() {
+    const { labelName, currentGrades } = this.props;
+    const { grades } = currentGrades;
     return (
       <div className={style.inputItem}>
-        <label>
-          Módulo 1 - Fundamentos (0 - 100)
-          <input
-            type="number"
-            onInput={this.handleInputsChange}
-            name="fundamentos"
-            value={this.state.firstInput}
-            min="0"
-            max="100"
-          />
-        </label>
-        <label>
-          Módulo 2 - Angular (0 - 100)
-          <input
-            type="number"
-            onInput={this.handleInputsChange}
-            name="angular"
-            value={this.secondInput}
-            min="0"
-            max="100"
-          />
-        </label>
-        <label>
-          Módulo 3 - React (0 - 100)
-          <input
-            type="number"
-            onInput={this.handleInputsChange}
-            name="react"
-            value={this.thirthInput}
-            min="0"
-            max="100"
-          />
-        </label>
-        <label>
-          Módulo 4 - Vue (0 - 100)
-          <input
-            type="number"
-            onInput={this.handleInputsChange}
-            name="vue"
-            value={this.firstInput}
-            min="0"
-            max="100"
-          />
-        </label>
-        <label>
-          Módulo 5 - Desafio Final (0 - 100)
-          <input
-            type="number"
-            onInput={this.handleInputsChange}
-            name="desafio"
-            value={this.fivethInput}
-            min="0"
-            max="100"
-          />
-        </label>
+        <label>{labelName}</label>
+        <input
+          type="number"
+          onInput={this.handleInputsChange}
+          min="0"
+          max="100"
+          defaultValue={grades}
+        />
       </div>
     );
   }
